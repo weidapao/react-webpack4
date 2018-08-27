61582cd0fb45cfeaac63c15905f5a0e40af8b511
Eager <1226393396@qq.com>
2018-08-27 21:45:40 +0800

    配置热更新，增加常用loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'antd'
-
+import style from './index.css'
+// 还需要在主要的js文件里写入下面这段代码
+if (module.hot) {
+  // 实现热更新
+  module.hot.accept()
+}
 const Index = () => {
   return (
-    <div>
-      <Button type="danger">1</Button>
-      <Button type="dashed">23</Button>
+    <div className={style.background} style={{ background: 'green' }}>
+      <Button type="danger">9999</Button>
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,69 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path')
+let CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+let ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
+let webpack = require('webpack');
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html"
-});
+  template: './src/index.html',
+  filename: './index.html'
+})
 
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundled.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    host: 'localhost', // 默认是localhost
+    port: 3000, // 端口
+    open: true, // 自动打开浏览器
+    hot: true // 开启热更新
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
       },
       {
         test: /\.css$/,
+        use: ExtractTextWebpackPlugin.extract({
+          // 将css用link的方式引入就不再需要style-loader了
+          use: ['css-loader', 'postcss-loader']
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
         use: [
           {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader",
+            loader: 'url-loader',
             options: {
-              // modules: true,  // antd必须去掉此项才有样式
-              importLoaders: 1,
-              localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-              minimize: true
+              limit: 8192, // 小于8k的图片自动转成base64格式，并且不会存在实体图片
+              outputPath: 'images/' // 图片打包后存放的目录
             }
           }
         ]
+      },
+      {
+        test: /\.(htm|html)$/,
+        use: 'html-withimg-loader'
+      },
+      {
+        test: /\.(eot|ttf|woff|svg)$/,
+        use: 'file-loader'
       }
     ]
   },
-  plugins: [htmlWebpackPlugin]
-};
+  plugins: [
+    new CleanWebpackPlugin('dist'),
+    htmlWebpackPlugin,
+    new ExtractTextWebpackPlugin('css/style.css'),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}

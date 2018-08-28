81e7ae1bf6f90c2d5ed338adf1d329d5cfb9c0ac
Eager <1226393396@qq.com>
2018-08-28 23:42:17 +0800

    feat: 增加babelrc转换class插件

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'antd'
 import style from './index.css'
+import App from './App'
 // 还需要在主要的js文件里写入下面这段代码
 if (module.hot) {
   // 实现热更新
   module.hot.accept()
 }
 const Index = () => {
-  return (
-    <div className={style.background} style={{ background: 'green' }}>
-      <Button type="danger">999</Button>
-      <Button type="dashed">1111</Button>
-    </div>
-  )
+  return <App />
 }
 
 ReactDOM.render(<Index />, document.getElementById('index'))
